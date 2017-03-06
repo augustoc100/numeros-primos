271d65dfc2827262538676bec9ba427982764e3b
carlos <caugustoc100@hotmail.com>
2017-03-06 15:02:33 -0300

    second commit

--- prime.rb
+++ prime.rb
@@ -4,41 +4,60 @@ def pegaPrimo(min, max)
 	while minimo <= max
 
 	 	numeros = 0
-	 	limite = minimo
-
-	 	while limite > 1
-	 		  numeros +=1 if minimo%limite==0	 		 	
-	 		  break if numeros > 1
-	 		 limite-=1
+	 	raiz = Math.sqrt(minimo)
+	 	limite = raiz.to_i 
+	 	comeco = 2
+	 	while comeco < limite + 1
+	 		  if minimo%comeco==0 	
+	 		  	numeros += 1
+	 		  	break
+	 		  end
+	 		  	 
+	 		 comeco+=1
 	 	end
 
-	 		 puts minimo if numeros == 1
-	 			
+	 		puts minimo  if numeros < 1 
+	 			 
+	 		
 		minimo+=1
 	end
 	puts 
 	
 end
-num = gets 
 
-while num
-	numTeste = num.split
+
 		
-		if numTeste.length == 1
-			break if numTeste[0].to_i >=10
-			numDeOcorrencias = numTeste
-		else
-			min = numTeste[0].to_i
-			max = numTeste[1].to_i
+def arruma(i)
+	i.times do |x|
+		num = gets
+		numero = num.split
+		break if numero ==1
+
+		    min = numero[0].to_i
+			max = numero[1].to_i
 			minTermos = min>=1 && min <= max
 			maxTermos = max >= min && max<= 1000000000
 			termosDiff = (max - min) <=100000 
 			break if !minTermos || !maxTermos || !termosDiff
+			min = 2 if min == 1
 			pegaPrimo(min,max)
-		end 
-		
-	num = gets
+	end
 	
 end
+ 
 
+num = gets 
+j=0
+ocorrencias = 10
+  while num
+	   if num.split.length == 1
+	   	    ocorrencias = num.split[0].to_i
 
+	   	arruma(ocorrencias)
+	   else
+	   	break
+	   end
+
+			
+			num = gets 
+       end
